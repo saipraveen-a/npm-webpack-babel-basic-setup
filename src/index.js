0ddb72a3e24099c881bab5ecfbef5304739ad526
Sai Praveen Anumukonda <saipraveen-a@github.com>
2020-09-19 14:08:56 +0530

    Add js and html starter files

---
+++ src/index.js
@@ -0,0 +1,12 @@
+let word1 = 'Sai';
+let word2 = 'Praveen';
+let num1 = 2;
+let num2 = 3;
+
+// const fullName = `${num1 + num2} ${word2}`;
+let example = `${word1} ${word2}`;
+// console.log(fullName);
+console.log(example)
+
+var paragraph = document.querySelector('#example');
+paragraph.innerText = example;
